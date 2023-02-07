41a2d8fee96a6a27d6e6ad184ec3678a93ab77d1
Antoine Thomas <ton.io.webagence@gmail.com>
2023-02-07 18:22:18 +0100

    Ajout data des produits & mise en page galerie produits page d'accueil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header>
         <a href="/">amazona</a>
       </header>
       <main>
-        list items
+        <h1>Featured Products</h1>
+
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p><strong>{product.name}</strong></p>
+                </a>
+                <p>{product.price} €</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
-    
+
   );
 }
 
